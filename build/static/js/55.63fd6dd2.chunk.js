e506f0d844502c4635c74fa47ae949a330ce2b28
Tushar-757 <tushar.1980.tv@gmail.com>
2021-10-30 21:34:59 +0530

    my first move towards cloudflare

--- build/static/js/55.63fd6dd2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpplant-app"]=this["webpackJsonpplant-app"]||[]).push([[55],{338:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var i=e(27),o=e(25),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=55.63fd6dd2.chunk.js.map
